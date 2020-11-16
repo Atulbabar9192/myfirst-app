b16358fe4a6330148b0c6f27b647faeadd49e3ec
Atulbabar9192 <Atulbabar9192@users.noreply.github.com>
2020-11-16 02:48:20 -0800

    commit

--- src/component/counter.jsx
+++ src/component/counter.jsx
@@ -1,9 +1,27 @@
-import React, { Component } from 'react';   
-
+import React, { Component } from 'react';  
+    
 class Counter extends Component {
-    render() { 
-        return (<div> <h1>Hello progerammers...</h1> <button>Increment</button></div>);
-    }
+    
+    state={
+ count:2,
+ name:"Rahul",
+ tags=["THis is java", "This is .net", "This  is c++", "This is react"]
+
 }
- 
+    render() { 
+        {
+let classes = "badge m-2 badge-";
+
+classes+= this.state.count==0 ? "warning" :"primary" ;
+
+    return (<React.Fragment> <title>Hello progerammers...</title> 
+    <span>{this.state.count}</span>
+    <p>we can add the two nos</p>
+    <ul> {this.state.tags.map( name=><li key={tag}>{tag}</li>)} </ul>
+    <span className= {classes}>{this.state.name}</span>
+    <button className="btn btn-warning btn-sm" > Increment </button></React.Fragment>);
+    
+    }
+ }
+}
 export default Counter ;
