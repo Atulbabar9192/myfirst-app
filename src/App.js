68da7300295d5e0bf2da8ff1b66023729579e3b9
Atulbabar9192 <Atulbabar9192@users.noreply.github.com>
2021-01-23 08:53:18 -0800

    new tour site created

--- src/App.js
+++ src/App.js
@@ -1,54 +1,12 @@
-import './App.css';
-// import Header from './component/Header';
-import { Button, Col, Container, Row }  from  'reactstrap';
-import { ToastContainer,toast } from 'react-toastify';
-import Home from './component/Home'
-import Course from './component/Course';
-import Allcourses from './component/Allcourses';
-import AddCourse from './component/AddCourse';
-import Header from './component/Header';
-import Menus from './component/Menus';
-import {BrowserRouter as Router,Route} from "react-router-dom";
+import React from 'react'
+import './App.css'
 
 function App() {
-    const btnHandle=()=>
-    
-    {
-      toast("This is my first message" ,{position:"top-center"}) ;
-      toast.error('done',{position:"top-center"});
-    };
-    
-
   return (
-    <div>
-      <Router>
-     <ToastContainer/>
-           {/* <Home/>
-           <Allcourses/>   
-           <AddCourse/>
-            */}
-            <Container>
-              <Header/>
-              <Row>
-                <Col md={4}>
-                  <Menus/>
-                  
-                </Col>
-                <Col md={8}>
-                  {/* Routes created for routing the app or component */}
-                  <Route path="/home" component={Home} exact></Route>
-                  <Route path="/add-course" component={AddCourse} exact></Route>
-                  <Route path="/view-courses" component={Allcourses} exact></Route>
-                  <Route path="/about-us" component={""} exact></Route>
-                  <Route path="/contact-us" component={""} exact></Route>
 
-
-
-                </Col>
-              
-              </Row>
-            </Container>
-            </Router></div>
-  );
+    <div className='App'>
+      < h1> Welcome to the Dream Tours </h1 >
+    </div >
+  )
 }
 export default App;
